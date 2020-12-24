48ad6863a7689ce5dfb32bb9ebbf524374b4454d
Stefan Kodzhabashev <stefan.kodzhabashev@psy.unibe.ch>
2020-12-24 16:16:42 +0100

    add loader while the app strucutre is fetched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,45 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
+import { LoadingController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { name } from '../../package.json';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private loadingCtrl: LoadingController
+    ) {
+        this.initializeApp();
+        this.presentLoadingWithOptions();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
+
+    async presentLoadingWithOptions() {
+        const loading = await this.loadingCtrl.create({
+            // message: '<ion-img src="/assets/loading.gif" alt="loading..."></ion-img>',
+            message: '<div class="loader">Loading ' + name + '...</div>',
+            cssClass: 'scale-down-center',
+            translucent: true,
+            showBackdrop: false,
+            spinner: null,
+            duration: 3000
+        });
+        await loading.present();
+
+        const { role, data } = await loading.onDidDismiss();
+        console.log('Loading dismissed with role:', role);
+    }
 }
