2f0d52ec1feb5c4d4d147ffb2e902ee93b0d6080
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-05-04 09:21:59 +0200

    load theme from the style selection from the ms

--- src/app/styles/survey-js-style/survey-js-style.component.ts
+++ src/app/styles/survey-js-style/survey-js-style.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { BasicStyleComponent } from '../basic-style/basic-style.component';
 import { SurveyJSStyle } from 'src/app/selfhelpInterfaces';
 import { SelfhelpService } from 'src/app/services/selfhelp.service';
-import { Model } from "survey-core";
+import { Model, StylesManager  } from "survey-core";
 
 @Component({
     selector: 'app-survey-js-style',
@@ -19,8 +19,32 @@ export class SurveyJSStyleComponent extends BasicStyleComponent implements OnIni
 
     ngOnInit(): void {
         console.log(this.style);
-        const survey = new Model(this.style.survey_json);
-        this.surveyModel = survey;
+        console.log(this.url);
+        StylesManager.applyTheme(this.getFieldContent('survey-js-theme'));
+        this.surveyModel = new Model(this.style.survey_json);
+    }
+
+    private saveSurveyJS(surveyFields, survey) {
+        let data = { ...survey.data };
+        data.pageNo = survey.currentPageNo;
+        if (!surveyFields['restart_on_refresh'] && data['survey_generated_id']) {
+            window.localStorage.setItem(data['survey_generated_id'], JSON.stringify(data));
+        }
+        data['_json'] = JSON.stringify(data);
+        $.ajax({
+            type: 'post',
+            url: this.url,
+            data: data,
+            success: function (r) {
+                if (data['trigger_type'] == 'finished') {
+                    // on successful save on completed survey remove the local storage data
+                    window.localStorage.removeItem(data['survey_generated_id']);
+                    if (surveyFields['redirect_at_end']) {
+                        window.location.href = surveyFields['redirect_at_end'];
+                    }
+                }
+            }
+        });
     }
 
 }
