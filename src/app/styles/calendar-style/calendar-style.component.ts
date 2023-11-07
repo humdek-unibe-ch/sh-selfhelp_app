ff9f886f80a8334e875fb5fbe3e0217dea55da4b
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-11-07 14:16:03 +0100

    load events in the calendar and set the options

--- src/app/styles/calendar-style/calendar-style.component.ts
+++ src/app/styles/calendar-style/calendar-style.component.ts
@@ -1,40 +1,9 @@
-import { Component, Input, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef, signal } from '@angular/core';
-import { Calendar } from '@awesome-cordova-plugins/calendar/ngx';
-import { Platform } from '@ionic/angular';
-import { CalendarComponent, CalendarMode } from 'ionic2-calendar';
+import { Component, Input, OnInit } from '@angular/core';
 import { CalendarStyle } from 'src/app/selfhelpInterfaces';
 import { BasicStyleComponent } from '../basic-style/basic-style.component';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { CalendarOptions } from '@fullcalendar/core';
-
-// interface CalendarOptions {
-//     calendarMode: CalendarMode;
-//     currentDate: Date;
-//     formatDay: string;
-//     formatDayHeader: string;
-//     formatDayTitle: string;
-//     formatWeekTitle: string;
-//     formatMonthTitle: string;
-//     formatWeekViewDayHeader: string;
-//     formatHourColumn: string;
-//     showEventDetail: boolean;
-//     startingDayMonth: number;
-//     startingDayWeek: number;
-//     allDayLabel: string;
-//     noEventsLabel: string;
-//     step: number;
-//     timeInterval: number;
-//     autoSelect: boolean;
-//     dir: string;
-//     scrollToHour: number;
-//     preserveScrollPosition: boolean;
-//     lockSwipeToPrev: boolean;
-//     lockSwipes: boolean;
-//     locale: string;
-//     startHour: number;
-//     endHour: number;
-//     sliderOptions: Record<string, any>;
-//   }
+import { CalendarOptions, EventSourceInput } from '@fullcalendar/core';
+import { addDays, format } from 'date-fns';
 
 @Component({
     selector: 'app-calendar-style',
@@ -47,161 +16,194 @@ export class CalendarStyleComponent extends BasicStyleComponent implements OnIni
     eventSource: any[] = [];
     viewTitle!: string;
     selectedDate: Date = new Date();
-
-    // calendar: CalendarOptions = {
-    //     calendarMode: 'month',
-    //     currentDate: new Date(),
-    //     formatDay: 'dd',
-    //     formatDayHeader: 'EEE',
-    //     formatDayTitle: 'MMMM dd, yyyy',
-    //     formatWeekTitle: 'MMMM yyyy, Week, w',
-    //     formatMonthTitle: 'MMMM yyyy',
-    //     formatWeekViewDayHeader: 'EEE d',
-    //     formatHourColumn: 'ha',
-    //     showEventDetail: true,
-    //     startingDayMonth: 0,
-    //     startingDayWeek: 0,
-    //     allDayLabel: 'all day',
-    //     noEventsLabel: 'No Events',
-    //     step: 60,
-    //     timeInterval: 60,
-    //     autoSelect: true,
-    //     dir: "",
-    //     scrollToHour: 0,
-    //     preserveScrollPosition: false,
-    //     lockSwipeToPrev: false,
-    //     lockSwipes: false,
-    //     locale: 'de-DE',
-    //     startHour: 0,
-    //     endHour: 24,
-    //     sliderOptions: {},
-    // }
-
     calendars = [];
     events = [];
     dataRange: any;
 
     calendarOptions: CalendarOptions = {
         initialView: 'dayGridMonth',
+        themeSystem: 'bootstrap',
         plugins: [dayGridPlugin]
-      };
+    };
 
-    @ViewChild(CalendarComponent) myCal!: CalendarComponent;
-
-    constructor(
-        private nativeCal: Calendar,
-        private plt: Platform
-    ) {
+    constructor() {
         super();
-        // this.nativeCal.listCalendars().then(data => {
-        //     this.calendars = data;
-        // });
     }
 
     override ngOnInit() {
-        // let config = this.getFieldContent('config');
-        // if (config) {
-        //     for (let key of Object.keys(config)) {
-        //         // @ts-ignore
-        //         this.calendar[key] = config[key];
-        //     }
-        // }
+        console.log("calendar", this.style);
+        this.setCalendarOptions();
     }
 
-    // Change current month/week/day
-    next() {
-        // this.myCal.slideNext();
+    setCalendarOptions() {
+        let calendar_data = this.style['calendar_values'];
+        let events = this.style['events'];
+        console.log('calendar',events);
+        let buttons = this.get_custom_buttons(calendar_data);
+        this.calendarOptions = {
+            initialView: 'dayGridMonth',
+            themeSystem: 'bootstrap',
+            plugins: [dayGridPlugin],
+            locale: calendar_data['locale'],
+            headerToolbar: {
+                left: buttons['buttons'],
+                // center: 'title',
+                right: 'dayGridMonth,dayGridWeek,dayGridDay,listWeek'
+            },
+            customButtons: buttons['customButtons'],
+            buttonText: {
+                today: calendar_data['label_today'],
+                day: calendar_data['label_day'],
+                week: calendar_data['label_week'],
+                month: calendar_data['label_month'],
+                list: calendar_data['label_list']
+            },
+            eventTimeFormat: {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false, // Set to true for 12-hour format with AM/PM
+                // hourCycle: `h23`
+            },
+            eventOverlap: true,
+            weekNumbers: true,
+            weekNumberFormat: {
+                week: 'short'
+            },
+            // height: 'auto',
+            firstDay: 1,
+            events: this.prepare_events(events, calendar_data['config']),
+        };
     }
 
-    back() {
-        // this.myCal.slidePrev();
-    }
-
-    // Selected date range and hence title changed
-    onViewTitleChanged(title: any) {
-        // this.viewTitle = title;
-    }
-
-    // Calendar event was clicked
-    async onEventSelected() {
-        // if (this.plt.is('ios')){
-        //     this.nativeCal.modifyEvent(event.title, event.location, event.notes, event.startDate, event.endDate, 'new title', event.location, event.notes, event.startDate, event.endDate);
-        // }else {
-        // this.nativeCal.openCalendar(this.selectedDate).then(
-        //     (msg) => { console.log(msg); },
-        //     (err) => { console.log(err); }
-        // );
-        // }
-    }
-
-    changeMode(mode: CalendarMode) {
-        // this.calendar.calendarMode = mode;
-    }
-
-    onRangeChanged(event: any) {
-        // this.dataRange = event;
-        // if (this.plt.is('ios')) {
-        //     // this.nativeCal.findAllEventsInNamedCalendar("stefan.kodzhabashev@gmail.com").then(data => {
-        //     //     this.events = data;
-        //     // });
-        //     let start = event.startTime;
-        //     let end = event.endTime;
-        //     this.nativeCal.listEventsInRange(start, end).then(data => {
-        //         this.events = data;
-        //         this.loadEventsInCalendar(data);
-        //     });
-        // } else if (this.plt.is('android')) {
-        //     let start = event.startTime;
-        //     let end = event.endTime;
-
-        //     this.nativeCal.listEventsInRange(start, end).then(data => {
-        //         this.events = data;
-        //         this.loadEventsInCalendar(data);
-        //     });
-        // }
-    }
-
-    loadEventsInCalendar(events: any) {
-        // this.eventSource = [];
-        // const isIOS = this.plt.is('ios');
-        // events.forEach((event: any) => {
-        //     if (!isIOS) {
-        //         event['allDay'] = event.allDay == 1;
-        //     } else {
-        //         // event['allDay'] = moment(event.endDate).format('HH:mm:ss') === '23:59:59';
-        //     }
-        //     event['endTime'] = new Date(isIOS ? new Date(event.endDate).getTime() : new Date(event.dtend).getTime());
-
-        //     event['endTime'] = new Date(isIOS ? new Date(event.endDate).getTime() : event.dtend);
-        //     event['startTime'] = new Date(isIOS ? new Date(event.startDate).getTime(): event.dtstart);
-        //     this.eventSource.push(event);
-        // });
-    }
-
-    addEvent() {
-        // this.nativeCal.createEventInteractively('', '', '', this.selectedDate, this.selectedDate).then(res => {
-        //     this.onRangeChanged(this.dataRange);
-        // }, err => {
-        //     console.log('err: ', err);
-        // });
-    }
-
-    onCurrentDateChanged(ev: any) {
-        this.selectedDate = ev;
-    }
-
-    getBtnSize(): number {
-        let buttonsNumbers = 0;
-        if (this.getFieldContent('label_month')) {
-            buttonsNumbers++;
+    get_custom_buttons(calendar_data: any) {
+        var buttons = 'prev,next,today,addEventButton';
+        if (calendar_data['show_add_calendar_button'] == '1') {
+            // show add calendar btn if enabled
+            buttons = 'prev,next,today,addCalendarButton,addEventButton';
         }
-        if (this.getFieldContent('label_week')) {
-            buttonsNumbers++;
+        var res = {
+            customButtons: {
+                addEventButton: {
+                    text: calendar_data['label_calendar_add_event'],
+                    click: () => {
+                        alert('asd')
+                    }
+                },
+                addCalendarButton: {
+                    text: calendar_data['label_add_calendar'],
+                    click: function () {
+                        // addNewCalendar();
+                    }
+                }
+            },
+            buttons: buttons
         }
-        if (this.getFieldContent('label_day')) {
-            buttonsNumbers++;
+        console.log(res)
+        return res;
+    }
+
+    /**
+ * Prepare the  events for display in the calendar.
+ * @function
+ * @param {Array} events - The  events to be displayed in the calendar.
+ * @param {Object} config - The  calendar config
+ * @returns {Array} - The formatted events.
+ */
+    prepare_events(events: any, config: any) {
+        if (!config) {
+            // if no config return events
+            return events;
         }
-        return buttonsNumbers == 0 ? 0 : 12 / buttonsNumbers;
+        var configEvents = config['events'];
+        events.forEach((event: { [x: string]: any; }) => {
+            if (configEvents) {
+                Object.keys(configEvents).forEach(key => {
+                    if (event[configEvents[key]]) {
+                        event[key] = event[configEvents[key]];
+                    }
+                });
+            }
+            if (!event['className']) {
+                event['className'] = [];
+            } else {
+                event['className'] = [event['className']];
+            }
+            if (config['css']) {
+                // there is a global css for the event object
+                event['className'].push(config['css']);
+            }
+            if (config['form_calendars']) {
+                // there is calendars setup, check for colors
+                if (event['calendar_info'] && event['calendar_info']['color']) {
+                    event['backgroundColor'] = event['calendar_info']['color'];
+                    event['borderColor'] = event['calendar_info']['color'];
+                    if (this.isColorLight(event['calendar_info']['color'])) {
+                        event['textColor'] = "black";
+                        event['borderColor'] = "black";
+                    }
+                }
+            }
+            event['className'].push(event['record_id']);
+            if (!event['start']) {
+                event['start'] = event['edit_time'];
+            }
+            if (event['end']) {
+                let timePattern = /\d{2} \d{2}/;
+                if (!timePattern.test(event['end'])) {
+                    // there is no time add extra date because the end date is exclusive
+                    let parsedDate = new Date(event['end']);
+                    let updatedDate = addDays(parsedDate, 1);
+                    event['end'] = format(updatedDate, 'yyyy-MM-dd');
+                }
+            }
+        });
+        console.log('calendar',events);
+        return events;
+    }
+
+    /**
+ * Check if the color is light
+ * @function
+ * @param {String} color - The color code
+ * @returns {Boolean} - True if light color
+ */
+    isColorLight(color: String) {
+        // Remove any whitespace and convert to lowercase for consistent formatting
+        color = color.replace(/\s/g, '').toLowerCase();
+
+        // Check if the color starts with '#' (hexadecimal notation)
+        if (color.charAt(0) === '#') {
+            // Extract the hex values for red, green, and blue
+            const r = parseInt(color.slice(1, 3), 16);
+            const g = parseInt(color.slice(3, 5), 16);
+            const b = parseInt(color.slice(5, 7), 16);
+
+            // Calculate the perceived brightness using the YIQ formula
+            const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+            // You can adjust this threshold to your preference (e.g., 128 for a mid-level threshold)
+            return brightness > 128;
+        }
+
+        // Check if the color starts with 'rgb' (RGB notation)
+        if (color.startsWith('rgb(')) {
+            // Extract the RGB values using regular expressions
+            const rgb = color.match(/\d+/g);
+            if (rgb && rgb.length === 3) {
+                const r = parseInt(rgb[0]);
+                const g = parseInt(rgb[1]);
+                const b = parseInt(rgb[2]);
+
+                // Calculate the perceived brightness using the YIQ formula
+                const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+                // You can adjust this threshold to your preference (e.g., 128 for a mid-level threshold)
+                return brightness > 128;
+            }
+        }
+
+        // If the color format is not recognized, assume it's not light
+        return false;
     }
 
 }
