eb4acde39794ba265e31e3755c6ad2f3e2ee3352
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-11-09 16:08:22 +0100

    properly load date values in date input

--- src/app/styles/input-style/input-style.component.ts
+++ src/app/styles/input-style/input-style.component.ts
@@ -19,19 +19,19 @@ export class InputStyleComponent extends BasicStyleComponent implements OnInit {
     }
 
     override ngOnInit() {
-        console.log(this.getFieldContent('name'));
-        if(this.getFieldContent('name') === 'datetime' || this.getFieldContent('name') === 'date'){
-            this.parentForm.controls[this.getFieldContent('name')].setValue(formatISO(new Date(this.parentForm.controls[this.getFieldContent('name')].value)));
+        if (this.getFieldContent('type_input') === 'datetime' || this.getFieldContent('type_input') === 'date') {
+            if (this.parentForm.controls[this.getFieldContent('name')].value) {
+                this.parentForm.controls[this.getFieldContent('name')].setValue(formatISO(new Date(this.parentForm.controls[this.getFieldContent('name')].value)));
+            }
         }
     }
 
-    formatDate(controlName: any){
-        console.log(controlName);
+    formatDate(controlName: any) {
         this.parentForm.controls[controlName].setValue(this.parentForm.controls[controlName].value.split('T')[0]);
     }
 
-    formatDateTime(controlName: any){
-        if(this.parentForm.controls[controlName].value.includes('+')){
+    formatDateTime(controlName: any) {
+        if (this.parentForm.controls[controlName].value.includes('+')) {
             let time = this.parentForm.controls[controlName].value.split('+')[0];
             console.log(time.split('T')[0] + " " + time.split('T')[1]);
             this.parentForm.controls[controlName].setValue(time.split('T')[0] + " " + time.split('T')[1]);
