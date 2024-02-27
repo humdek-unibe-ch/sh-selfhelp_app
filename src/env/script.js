1c568a3db9fdfc9c25b5c14947bf596594d6f171
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-02-27 15:15:51 +0100

    adjust script to the new project name

--- src/env/script.js
+++ src/env/script.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var path = require('path');
 function copyEnvFileSync() {
     var fileData = fs.readFileSync(path.join('./configs/config.ts'), 'utf-8')
-    fs.writeFileSync('./SelfHelpMobile/src/env/app.config.ts', fileData, 'utf-8');
+    fs.writeFileSync('./sh-selfhelp_app/src/env/app.config.ts', fileData, 'utf-8');
     console.log('Environment file copied successfully')
 }
 copyEnvFileSync();
